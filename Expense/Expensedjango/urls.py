9ef4e4cd25133b8f01291fa92cc0c848fd33a3bb
Tejas <tkb7455@gmail.com>
2025-08-04 15:37:01 +0530

    transaction_list addded

--- Expense/Expensedjango/urls.py
+++ Expense/Expensedjango/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from Expensedjango.views import RegisterView, DashboardView
+from Expensedjango.views import RegisterView, DashboardView, TransactionCreateView, TransactionListView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('', DashboardView.as_view(), name='dashboard'),
+    path('transaction/add/',TransactionCreateView.as_view(), name='transaction_add'),
+    path('transactions/', TransactionListView.as_view(), name="transactions"),
 ] 
